6830271077110dc3f15030bb2894ee30164dad96
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-02-17 13:26:41 +0100

    build: work with even older Node.js versions

---
+++ node-test.cjs
@@ -0,0 +1,4 @@
+// deno-lint-ignore no-var
+var names = require("./index.cjs");
+
+console.log("UNISEX", names.UNISEX[2]);
--- node-test.js
+++
@@ -1,4 +0,0 @@
-// deno-lint-ignore no-var
-var { UNISEX } = require("./index.cjs");
-
-console.log("UNISEX", UNISEX[2]);
